747e3817f41fc65c968e10811a2c4a16b8c7bdae
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-26 01:02:12 +0800

    chore: installed cors.

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: 'akira',
+    username: 'chuanxin',
     password: null,
-    database: 'climbing_development',
+    database: 'hrms_express_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
