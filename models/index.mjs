b1cba40f99485451ebf82844184dcbf6e16bfb62
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-26 02:03:48 +0800

    chore: heroku sequelize deploy settings.

--- models/index.mjs
+++ models/index.mjs
@@ -1,16 +1,35 @@
 import { Sequelize } from 'sequelize';
+import url from 'url';
 import allConfig from '../config/config.js';
 
 import initRouteModel from './route.mjs';
 import initTripModel from './trip.mjs';
 
 const env = process.env.NODE_ENV || 'development';
-
 const config = allConfig[env];
-
 const db = {};
+let sequelize;
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+// If env is production, retrieve database auth details from the
+// DATABASE_URL env var that Heroku provides us
+if (env === 'production') {
+  // Break apart the Heroku database url and rebuild the configs we need
+  const { DATABASE_URL } = process.env;
+  const dbUrl = url.parse(DATABASE_URL);
+  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
+  const password = dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length);
+  const dbName = dbUrl.path.slice(1);
+  const host = dbUrl.hostname;
+  const { port } = dbUrl;
+  config.host = host;
+  config.port = port;
+  sequelize = new Sequelize(dbName, username, password, config);
+}
+
+// If env is not production, retrieve DB auth details from the config
+else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 // add your model definitions to db here
 db.sequelize = sequelize;
