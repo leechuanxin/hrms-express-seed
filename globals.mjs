d32662a4486c88871242e5c0400be7254bf4cff1
Lee Chuan Xin <chuanxin@gametize.com>
2021-12-02 04:34:34 +0800

    fix(globals): removed INVALID_SETTINGS_REQUEST_MESSAGE.

--- globals.mjs
+++ globals.mjs
@@ -2,7 +2,6 @@
 export const USERNAME_EXISTS_ERROR_MESSAGE = 'Username exists!';
 export const LOGIN_FAILED_ERROR_MESSAGE = 'Login failed!';
 export const INVALID_REGISTER_REQUEST_MESSAGE = 'Invalid registration!';
-export const INVALID_SETTINGS_REQUEST_MESSAGE = 'Invalid changing of user profile!';
 export const SETTINGS_USER_NO_EXIST_ERROR_MESSAGE = 'This user does not exist!';
 export const INVALID_LOGIN_REQUEST_MESSAGE = 'Invalid login parameters!';
 
