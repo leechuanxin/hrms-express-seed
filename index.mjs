c2a42eaa6bc1136cdc0b836554e7716f9b3c25cc
Lee Chuan Xin <chuanxin@gametize.com>
2021-12-02 04:24:06 +0800

    style: deployment test commit comment.

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 import bindRoutes from './routes.mjs';
 
+// Deployment test
 // const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 const FRONTEND_URL = 'https://desolate-mesa-48890.herokuapp.com';
 // Initialise Express instance
